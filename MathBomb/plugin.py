1c2ea6c86ea8ccd1d3d3fa54a9f9accfa6703a50
waratte <waratte.ka@gmail.com>
2015-04-16 22:42:01 -0400

    D:

--- MathBomb/plugin.py
+++ MathBomb/plugin.py
@@ -195,7 +195,7 @@ class Mathbomb(callbacks.Plugin):
             
             pair = self._generate_equation()
             
-            while pair[0] != solution:
+            while pair[0] == solution:
                 pair = self._generate_equation()
                 
             return pair
